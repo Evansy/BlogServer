7cc218658d8519e69e7a84971ba366e61333ef0e
Evan <skcy@vip.qq.com>
2017-12-15 01:07:54 +0800

    * 修改默认路由

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,7 @@ export default new Router({
     routes: [
         {
             path: '/',
-            redirect: '/views'
+            redirect: '/login'
         },
         {
             path: '/login',
